490a321990abb4cb383a1a42fa69919e39f9855b
junlongsun <junlong.sun@gmail.com>
2015-12-08 11:23:22 -0700

    finish bases

--- project/testClassifier.py
+++ project/testClassifier.py
@@ -17,7 +17,7 @@ data2015 = list(DictReader(open("pbp-2015.csv", 'r')))
 dataList = [data2013, data2014, data2015]
 dataName = ["2013","2014","2015"]
 
-o = DictWriter(open("output.csv", 'w'), ["dataName", "classifier", "percent", "score", "OmniScore", "Type1-A/A/Good","Type2-A/B/Bad",  "Type3-A/B/Good", "Type4-A/A/Bad"])
+o = DictWriter(open("Bayes.csv", 'w'), ["dataName", "classifier", "percent", "score", "OmniScore", "Type1-A/A/Good","Type2-A/B/Bad",  "Type3-A/B/Good", "Type4-A/A/Bad"])
 o.writeheader()
 
 #---------------------------------#
